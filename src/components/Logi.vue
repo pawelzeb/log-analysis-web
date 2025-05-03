c0f724a2483e4abd3c06bd69ddcf666933da5f9a
Paweł Żebrowski <piksell2@gmail.com>
2025-05-03 13:26:50 +0200

    axios ...

---
+++ src/components/Logi.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const logi = ref<any>(null);
+
+    onMounted(async () => {
+      try {
+        const res = await axios.get('http://localhost:5044/test');
+        logi.value = res.data;
+      } catch (err) {
+        console.error('Błąd przy pobieraniu logów:', err);
+      }
+    });
+
+    return { logi };
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Logi</h1>
+    <pre>{{ logi }}</pre>
+  </div>
+</template>
